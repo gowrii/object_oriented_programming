1d84234c9966dfc1660169a3f92401f8cba184d1
Gowri Nambiar <gowri.nambi@gmail.com>
2013-10-23 16:08:12 -0400

    create instance student

--- people.rb
+++ people.rb
@@ -1,12 +1,29 @@
-class Student
+
+
+class Person
+	def initialize (name)
+		@name = name
+	end
+	def greeting
+		puts "Hi my name is #{@name}"
+	end
+end
+
+class Student < Person
 	def learn
 		puts "I get it!"
 	end
 end
 
 
-class Instructor
+class Instructor < Person
 	def teach
-		puts "Everyting in Ruby is an Object"
+	puts "Everyting in Ruby is an Object"
 	end
 end
+person = Instructor.new("Chris")
+person.greeting
+
+person = Student.new("Cristina")
+person.greeting
+
